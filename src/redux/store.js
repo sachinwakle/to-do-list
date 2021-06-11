876f26b670d85962e14ef645d4d1a91e48372f96
sachinwakle <sbwakle@hotmail.com>
2021-06-11 14:34:45 +0530

    retained theme and task details in local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,14 @@
 import { createStore } from "redux";
 import rootReducer from "./rootReducer";
+import { loadState, saveState } from "./localStorage";
+import throttle from "lodash/throttle";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, loadState());
 
-store.subscribe(() => console.log(store.getState().task));
+store.subscribe(
+  throttle(() => {
+    saveState({ theme: store.getState().theme, task: store.getState().task });
+  }, 500)
+);
 
 export default store;
